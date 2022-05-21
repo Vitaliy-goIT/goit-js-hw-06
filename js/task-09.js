ec9df4854fffc056ed8d17a407569320234c505d
Vitaliy Zhoglo <zhoglo.vitaliy@gmail.com>
2022-05-21 17:29:10 +0300

    task9 done!

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const buttonEl = document.querySelector('.change-color');
+const colorValue = document.querySelector('.color');
+const bgcBody = document.querySelector('body');
+
+buttonEl.addEventListener('click', () => {
+  bgcBody.style.backgroundColor = getRandomHexColor();
+  colorValue.textContent = getRandomHexColor();
+});
