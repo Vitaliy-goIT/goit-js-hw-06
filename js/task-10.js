e2a0a7635e3ad291850815f7c44016845be95fa2
Vitaliy Zhoglo <zhoglo.vitaliy@gmail.com>
2022-05-22 01:32:12 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,38 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+const inputEl = document.querySelector('#controls > input');
+const boxes = document.querySelector('#boxes');
+
+inputEl.addEventListener('change', event => {
+  const amount = Number(event.currentTarget.value);
+  console.log(amount);
+
+  if (amount > 0) {
+    createBtn.addEventListener('click', () => {
+      const create = function createBoxes(amount) {
+        for (let i = 1; i <= amount; i += 1) {
+          const box = document.createElement('div');
+          box.style.backgroundColor = getRandomHexColor();
+          box.style.width = 30 + 'px';
+          box.style.height = 30 + 'px';
+          boxes.append(box);
+          console.log(box);
+          break;
+        }
+      };
+      create(amount);
+    });
+  }
+
+  if (!Number.isInteger(amount)) {
+    alert('Введіть ціле додатнє число!');
+  }
+
+  if (amount < 0) {
+    alert('Введіть додатнє число!');
+  }
+});
