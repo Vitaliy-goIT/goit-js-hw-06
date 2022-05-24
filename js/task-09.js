216639fbf7942634d73bc7a4f5f188d65c936440
Vitaliy Zhoglo <zhoglo.vitaliy@gmail.com>
2022-05-24 22:39:49 +0300

    9 update +10 <--not end

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,7 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
 
 const buttonEl = document.querySelector('.change-color');
@@ -7,6 +9,7 @@ const colorValue = document.querySelector('.color');
 const bgcBody = document.querySelector('body');
 
 buttonEl.addEventListener('click', () => {
-  bgcBody.style.backgroundColor = getRandomHexColor();
-  colorValue.textContent = getRandomHexColor();
+  let color = getRandomHexColor();
+  bgcBody.style.backgroundColor = color;
+  colorValue.textContent = color;
 });
--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,7 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
 
 const createBtn = document.querySelector('[data-create]');
@@ -8,24 +10,26 @@ const inputEl = document.querySelector('#controls > input');
 const boxes = document.querySelector('#boxes');
 
 inputEl.addEventListener('change', event => {
-  const amount = Number(event.currentTarget.value);
+  let amount = Number(event.currentTarget.value);
   console.log(amount);
 
   if (amount > 0) {
     createBtn.addEventListener('click', () => {
-      let sizeBox = 0;
       const create = function createBoxes(amount) {
-        // for (let i = 1; i <= amount; i += 1) {
-        for (let i = inputEl.min; i <= amount; i += 1) {
-          let sizeBoxIncr = 30 + sizeBox;
+        let sizeBox = 30;
+        let sizeBoxIncr = 10;
+
+        for (let i = inputEl.min; i <= amount; ++i) {
+          console.log(i);
           const box = document.createElement('div');
+          // box parameters
           box.style.backgroundColor = getRandomHexColor();
-          box.style.width = `${sizeBoxIncr}px`;
-          box.style.height = `${sizeBoxIncr}px`;
+          box.style.width = `${sizeBox}px`;
+          box.style.height = `${sizeBox}px`;
+          // adding box
           boxes.append(box);
           console.log(box);
-          sizeBox = sizeBoxIncr;
-          // break;
+          sizeBox += sizeBoxIncr;
         }
       };
       create(amount);
